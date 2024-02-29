9cc306222ef301def8d7620edb5ad185b5947ebb
Gabriel Sales - Notebook <gabrielsalesj7@gmail.com>
2024-02-29 17:58:12 -0300

    Com o início da engine

---
+++ src/scripts/engine.js
@@ -0,0 +1,37 @@
+const emojis = [
+  "🐵",
+  "🐵",
+  "🐶",
+  "🐶",
+  "🐺",
+  "🐺",
+  "🐱",
+  "🐱",
+  "🦝",
+  "🦝",
+  "🦊",
+  "🦊"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuffleEmojis[i];
+  box.onclick = handleClick;
+  document.querySelector(".game").appendChild(box);
+}
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+
+}
